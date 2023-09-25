f89cd089c50026294bc7cf42871023f05d87b2a6
Asher Siegel <ashersiegel@icloud.com>
2023-09-24 21:45:27 -0500

    Initial Commit

---
+++ basicWeather/basicWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicWeatherApp.swift
+//  basicWeather
+//
+//  Created by Asher Siegel on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct basicWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
